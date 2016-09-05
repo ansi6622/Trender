8b0c76a92be3f77eecfd72ca5cf86e2a1f464f58
Anthony Simpson <ansi6622@colorado.edu>
2016-09-04 20:25:26 -0600

    trimming

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-
 var express = require('express');
 var path = require('path');
-var routes = require('./routes/index');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var http = require('http');
-// var swig = require('swig');
 var app = express();
-// swig = new swig.Swig();
-// app.engine('html', swig.renderFile);
-// app.set('view engine', 'html');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,9 +19,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -40,7 +31,6 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
@@ -50,6 +40,4 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-
 module.exports = app;
